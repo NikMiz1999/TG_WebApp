7ebaa48dac5fecd7bb1625efb1f5216752214dca
Nikita <nikitamizonov76@gmail.com>
2025-12-05 02:39:42 +0300

    Delete telewebapp directory

--- telewebapp/run-dev.ps1
+++
@@ -1,9 +0,0 @@
-# --- конфиг окружения для dev ---
-$env:BOT_TOKEN="7735626834:AAG2hoI1ShmH24lk2nk-hlnEZv2c0AK2Qsw"
-$env:GROUP_CHAT_ID="-1002620513089"
-$env:TIMESHEET_ID="1J212D9-n0eS5DnEST7JqObeE2S1umHCSRURjhntq4R8"
-$env:SESSION_SECRET="devsecret"
-
-# --- запуск сервера ---
-uvicorn app:app --reload --log-level info --access-log
-
